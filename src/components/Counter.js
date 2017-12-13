53228dde24b5f065577b3c00703ceee7b2f68204
Steve Occhipinti <dev@stevenocchipinti.com>
2017-12-14 00:59:04 +1100

    Spike working!

---
+++ src/components/Counter.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+const Counter = props => (
+  <div className="counter">{props.value}</div>
+)
+
+export default Counter
--- src/index.js
+++ src/index.js
@@ -1,8 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
+import firebase from 'firebase';
+
+firebase.initializeApp({
+  apiKey: "AIzaSyAfu0kXXviAaCCl2j8DTwBDlkANNZKkdkw",
+  authDomain: "fire-event-store-demo.firebaseapp.com",
+  databaseURL: "https://fire-event-store-demo.firebaseio.com",
+  projectId: "fire-event-store-demo",
+  storageBucket: "fire-event-store-demo.appspot.com",
+  messagingSenderId: "932085506803"
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
